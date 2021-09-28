35a254993b05acc91201158a0fe6b22516d02fa8
Mathmo <mathis.moyse-2@etu.univ-tours.fr>
2021-09-28 18:03:44 +0200

    un fichier CPersonne
    classe d'une personne

---
+++ CPersonne.py
@@ -0,0 +1,48 @@
+
+
+class CPersonne:
+
+    def __init__(self):
+        self._iPoids
+        self._fVitesse
+        self._fPression
+        self._lDirection = []
+        self._lCoordonees = []
+
+#------------------------Getter------------------------
+
+    def _get_iPoids(self):
+        return self._iPoids
+
+    def _get_fVitesse(self):
+        return self._iVitesse
+
+    def _get_fPression(self):
+        return self._fPression
+
+    def _get_lDirection(self):
+        return self._lDirection
+
+    def _get_lCoordonnees(self):
+        return self._lCoordonees
+
+#------------------------Setter------------------------
+
+    def _set_iPoids(self, poids):
+        self._iPoids = poids
+
+    def _set_fVitesse(self, vitesse):
+        self._fVitesse = vitesse
+
+    def _set_fPression(self, pression):
+        self._fPression = pression
+
+    def _self_lDirection(self, direction):
+        self._lDirection = direction
+
+    def _self_lCoordonnees(self, coordonnees):
+        self._lCoordonees = coordonnees
+
+#------------------------Methodes------------------------
+
+    def _
