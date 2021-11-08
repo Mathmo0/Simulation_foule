6a7bc3641c2afbb4708960372aec44250777eb16
Math12 <mathis.moyse-2@etu.univ-tours.fr>
2021-11-08 18:15:07 +0100

    code pour tester les formules

--- Test.py
+++ Test.py
@@ -1,4 +1,34 @@
-import socialforce
+import numpy as np
 
+list1 = [10, 15]
+list2 = [1, 5]
 
+a = np.array(list1)
+b = np.array(list2)
 
+dt = 0
+vitesse = 0
+
+def calculvitesse(a, b):
+    if(dt == 0):
+        return 0
+    else :
+        return np.linalg.norm((a - b), ord=1)/dt
+
+def vitesse(a, b):
+    if(dt == 0):
+        return 0
+    else :
+        return (a - b)/dt
+
+def nouvellepos(x, y):
+    return 2*(x - y)
+
+for i in range(0, 10):
+    print(a)
+    v = vitesse(a, b)
+    print(v)
+    ealpha = (b - a)/np.linalg.norm((a - b), ord=1)
+    print(ealpha)
+    a = a + nouvellepos(ealpha*1.34, v)
+    dt += 2
