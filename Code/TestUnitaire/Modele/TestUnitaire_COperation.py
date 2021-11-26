b72a2649a9115aa6b068af0e74db11dd60168b81
PolyLogiciel <maxime.edeline@etu.univ-tours.fr>
2021-11-26 20:41:40 +0100

    Update git ignore

--- Code/TestUnitaire/Modele/TestUnitaire_COperation.py
+++ Code/TestUnitaire/Modele/TestUnitaire_COperation.py
@@ -3,6 +3,4 @@ from Code.Modele.COperation import COpetation
 class TestUnitaire_COperation(unittest.TestCase) :
 
     def test_DetectionCercle_OK(self) :
-
-        var = COpetation.DetectionCercle(0,0,1,1,3)
-        self.assertTrue(var)
+        self.assertTrue(COpetation.DetectionCercle(0,0,1,1,3))
