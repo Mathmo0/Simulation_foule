082d3849a7f40915406b8a66227c58f32cece215
Mathmo <mathis.moyse-2@etu.univ-tours.fr>
2021-11-28 18:16:59 +0100

    etude du potentiel Pygame
    en bonne voie

--- Code/Modele/CObstacleQuadrilatere.py
+++ Code/Modele/CObstacleQuadrilatere.py
@@ -1,6 +1,10 @@
-from Code.Modele.CObstacle import CObstacle
-
+import pygame
+import sys
 import numpy as np
+import pygame
+
+from Modele.CObstacle import CObstacle
+
 
 class CObstacleQuadrilatere(CObstacle):
     """
@@ -8,12 +12,13 @@ class CObstacleQuadrilatere(CObstacle):
     """
 
     # -------------------Constructeur-------------------#
-    def __init__(self, nom = "", hauteur = 0, largeur = 0, coordonneesSommets = np.array([(0, 0)])):
+    def __init__(self, nom="", hauteur=0, largeur=0, coordonneesSommets=np.array([(0, 0)])):
         super().__init__(nom, coordonneesSommets)
         self.iSuperficie = hauteur * largeur
         self.iHauteur = hauteur
         self.iLargeur = largeur
-
+        self.rect = pygame.Rect(self.tCoordonneesSommet[0, 0], self.tCoordonneesSommet[0, 1], self.iLargeur,
+                                self.iHauteur)
 
     # -------------------Getters-------------------#
     def getHauteur(self):
@@ -21,7 +26,6 @@ class CObstacleQuadrilatere(CObstacle):
 
     def getLargeur(self):
         return self.iLargeur
-
 
     # ---------------------Setters---------------------#
     def setHauteur(self, hauteur):
@@ -32,17 +36,47 @@ class CObstacleQuadrilatere(CObstacle):
         self.iLargeur = largeur
         self.iSuperficie = self.iHauteur * self.iLargeur
 
-
     # -------------------Methodes-------------------#
     def OBSToString(self):
         print("\nNom : {}\n"
               "Coordonnees : {}\n"
-              "Superficie : {}"
+              "Superficie : {}\n"
               "Hauteur : {}\n"
               "Largeur : {}\n"
-              .format(self.getNom(), self.getCoordoneesSommet(), self.getSuperficie(), self.getHauteur(), self.getLargeur()))
+              .format(self.getNom(), self.getCoordoneesSommet(), self.getSuperficie(), self.getHauteur(),
+                      self.getLargeur()))
+
+    # -------------------Dessin-------------------#
+    def Dessin(self):
+        screen = pygame.display.set_mode((screen_width, screen_height))
+        screen.fill((255, 255, 255))
+        pygame.draw.rect(screen, (0, 0, 0), self.rect)
+        pygame.display.flip()
 
 
-list_sorties = np.array([(3, 4), (2, 4)])
-carreONE = CObstacleQuadrilatere("carreone", 12, 12, list_sorties)
+pygame.init()
+clock = pygame.time.Clock()
+screen_width, screen_height = 800, 800
+
+"""moving_rect = pygame.Rect(350, 350, 100, 100)"""
+x_speed, y_speed = 5, 4
+
+"""other_rect = pygame.Rect(300, 600, 200, 100)"""
+"""other_speed = 2"""
+list_sorties = np.array([(350, 350), (2, 4)])
+list_sorties2 = np.array([(300, 300), (2, 4)])
+carreONE = CObstacleQuadrilatere("carreone", 120, 120, list_sorties)
+carreTWO = CObstacleQuadrilatere("carretwo", 120, 120, list_sorties)
 carreONE.OBSToString()
+carreTWO.OBSToString()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+
+    clock.tick(60)
+    carreONE.Dessin()
+    carreTWO.Dessin()
