8944c30805aa08d597cf9329ac6a18cb8bbc4b51
hicham <hichamdudby@outlook.com>
2021-11-30 15:48:41 +0100

    Update CFichier et Update main

--- Code/Modele/CFichier.py
+++ Code/Modele/CFichier.py
@@ -1,4 +1,4 @@
-
+import csv
 
 class CFichier:
     """
@@ -7,7 +7,7 @@ class CFichier:
 
     # -------------------Constructeur-------------------#
     def __init__(self, nomFichier = ""):
-        self.sNomFichier = nomFichier
+        self.sNomFichier = nomFichier+".csv"
 
     # -------------------Getters-------------------#
     def getNomFichier(self):
@@ -20,3 +20,16 @@ class CFichier:
 
 
     # -------------------Methodes-------------------#
+    def LireFichier(self):
+        #Creation de la liste des positions
+        listPositions = []
+
+        #Ouverture du fichier
+        with open(self.sNomFichier, newline='') as csv_file:
+            reader = csv.reader(csv_file, delimiter=";")
+            next(csv_file)
+            for row in reader:
+                #Stockage des positions dans la liste
+                listPositions.append([float(x) for x in row])
+
+        return listPositions
---
+++ Code/main.py
@@ -0,0 +1,13 @@
+from Modele.CFichier import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+chemin = "C:/Users/Hicham/Desktop/"
+
+monFichier = CFichier(chemin+"oui")
+
+list = []
+
+list = monFichier.LireFichier()
+
+print(list)
