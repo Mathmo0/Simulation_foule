3395cd5f4d8033fd9a4c31c2916ce83cd87a91a4
Mathmo <mathis.moyse-2@etu.univ-tours.fr>
2021-11-30 16:53:17 +0100

    update : Environnements

--- Code/Environnements.py
+++ Code/Environnements.py
@@ -6,8 +6,6 @@ list_sorties2 = np.array([(3, 4), (2, 4)])
 list_sorties3 = np.array([(3, 4), (2, 4)])
 list_sorties4 = np.array([(3, 4), (2, 4)])
 
-
-
 e1 = CEnvironnement("Bureau", 34, 20, 3, 1, list_sorties)
 e2 = CEnvironnement("Chambre", 34, 20, 3, 1, list_sorties)
 e3 = CEnvironnement("Hall", 34, 20, 3, 1, list_sorties)
