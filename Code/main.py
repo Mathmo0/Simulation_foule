6ccc9be37fc4df16c401856e2c89c236236c7b8b
hicham <hichamdudby@outlook.com>
2021-12-13 17:38:55 +0100

    update main
    recréation de l'écriture dans un fichier

--- Code/main.py
+++ Code/main.py
@@ -2,6 +2,21 @@ from Code.Modele.CFichier import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+Mathis = CPersonne(np.array[0, 0])
+Maxime = CPersonne(np.array[0, 0])
+Hicham = CPersonne(np.array[0, 0])
+
+listPersonnes = []
+
+listPersonnes.append(Mathis)
+listPersonnes.append(Maxime)
+listPersonnes.append(Hicham)
+
+
+
+
+
+
 """Environnements.e1.ENVToString()
 Environnements.e2.ENVToString()
 Environnements.e3.ENVToString()
